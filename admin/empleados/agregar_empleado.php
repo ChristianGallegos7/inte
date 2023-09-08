aa355a22117d1fcff07109a714f1802dd41f3472
Christian <christiantenz7@gmail.com>
2023-09-07 23:00:56 -0500

    cerca

---
+++ admin/empleados/agregar_empleado.php
@@ -0,0 +1,85 @@
+<?php
+include '../../conexion.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["agregar_empleado"])) {
+    $nombre_empleado = $_POST["nombre_empleado"];
+    $email_empleado = $_POST["email_empleado"];
+    $password_empleado = $_POST["password_empleado"];
+    $rol_empleado = $_POST["rol_empleado"];
+
+    // Verifica si el correo electrónico del empleado ya está registrado
+    $sql = "SELECT * FROM Empleados WHERE email = '$email_empleado'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        echo "El correo electrónico ya está registrado para otro empleado.";
+    } else {
+        // Hash de la contraseña antes de almacenarla en la base de datos
+        $hashed_password = password_hash($password_empleado, PASSWORD_DEFAULT);
+
+        // Inserta los datos del nuevo empleado en la tabla Empleados
+        $sql = "INSERT INTO Empleados (nombre, email, password, rol) VALUES ('$nombre_empleado', '$email_empleado', '$hashed_password', '$rol_empleado')";
+
+        if ($conn->query($sql) === TRUE) {
+            header("Location: ../index.php"); // Redirigir a la página de administración después de agregar el empleado
+            exit;
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+    }
+}
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Agregar Nuevo Empleado</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+</head>
+
+<body>
+    <header>
+        <!-- place navbar here -->
+    </header>
+    <main>
+        <div class="container">
+            <h2>Agregar Nuevo Empleado</h2>
+            <form method="POST" action="agregar_empleado.php">
+                <div class="mb-3">
+                    <label for="nombre_empleado" class="form-label">Nombre:</label>
+                    <input type="text" class="form-control" name="nombre_empleado" required>
+                </div>
+                <div class="mb-3">
+                    <label for="email_empleado" class="form-label">Correo Electrónico:</label>
+                    <input type="email" class="form-control" name="email_empleado" required>
+                </div>
+                <div class="mb-3">
+                    <label for="password_empleado" class="form-label">Contraseña:</label>
+                    <input type="password" class="form-control" name="password_empleado" required>
+                </div>
+                <div class="mb-3">
+                    <label for="rol_empleado" class="form-label">Rol:</label>
+                    <input type="text" class="form-control" name="rol_empleado" required>
+                </div>
+                <button type="submit" class="btn btn-primary" name="agregar_empleado">Agregar Empleado</button>
+                <a name="" id="" class="btn btn-primary" href="index.php" role="button">Cancelar</a>
+            </form>
+        </div>
+    </main>
+    <footer>
+        <!-- place footer here -->
+    </footer>
+    <!-- Bootstrap JavaScript Libraries -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
---
+++ admin/empleados/index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Lista de Empleados</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+</head>
+
+<body>
+    <header>
+        <!-- place navbar here -->
+        <nav class="navbar navbar-expand navbar-light bg-light d-flex justify-content-between p-4 mx-3">
+            <div>
+                <img src="../../images/avatar.png" alt="" width="50px">
+            </div>
+            <div class="nav navbar-nav gap-4">
+                <a class="nav-item nav-link btn btn-info p-3" href="../index.php">Volver</a>
+                <a class="nav-item nav-link active btn btn-success text-white p-3" href="./agregar_empleado.php" aria-current="page">Agregar Empleados <span class="visually-hidden">(current)</span></a>
+            </div>
+        </nav>
+    </header>
+    <main class="container mt-4">
+        <h2 class="text-center">Lista de Empleados</h2>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Nombre</th>
+                    <th>Correo Electrónico</th>
+                    <th>Rol</th>
+                    <th>Acciones</th> <!-- Nueva columna para acciones -->
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                require_once("../../conexion.php");
+                $query = "SELECT nombre, email, rol FROM Empleados";
+                $result = mysqli_query($conn, $query);
+
+                while ($row = mysqli_fetch_assoc($result)) {
+                    echo '<tr>';
+                    echo '<td>' . $row["nombre"] . '</td>';
+                    echo '<td>' . $row["email"] . '</td>';
+                    echo '<td>' . $row["rol"] . '</td>';
+                    echo '<td>';
+                    echo '<a href="editar_empleado.php?id=ID_EMPLEADO" class="btn btn-primary mx-3">Editar</a>';
+                    echo '<a href="eliminar_empleado.php?id=ID_EMPLEADO" class="btn btn-danger">Eliminar</a>';
+                    echo '</td>';
+                    echo '</tr>';
+                }
+                ?>
+            </tbody>
+        </table>
+    </main>
+    <footer>
+        <!-- place footer here -->
+    </footer>
+    <!-- Bootstrap JavaScript Libraries -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous"></script>
+</body>
+
+</html>
